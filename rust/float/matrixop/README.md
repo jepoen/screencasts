356cd143e0252adb1eec777802ecf10809511245
Jens Poenisch <jens@ruessel.in-chemnitz.de>
2024-11-14 20:11:05 +0100

    Gauß algorithm

--- rust/float/matrixop/README.md
+++ rust/float/matrixop/README.md
@@ -46,3 +46,87 @@ Zusammenfassung:
 - Anzeige Matrix/Vektor
 - Matrix-Vektor-Multiplikation
 - Differenz zwischen Vektoren
+
+## Lösen Lineares Gleichungssystem
+
+Gegeben:
+
+$$
+\begin{pmatrix}
+  a_{0,0} & a_{0,1} & a_{0,2} \\
+  a_{1,0} & a_{1,1} & a_{1,2} \\
+  a_{2,0} & a_{2,1} & a_{2,2}
+\end{pmatrix} \cdot
+\begin{pmatrix}
+x_0 \\ x_1 \\ x_2
+\end{pmatrix}
+=
+\begin{pmatrix}
+r_0 \\ r_1 \\ r_2
+\end{pmatrix}
+$$
+
+Gesucht: $(x_1, x_2, x_3)^T$
+
+Algorithmus: *Gauß-Algorithmus*
+
+~~~
+  5  7 -8  4 -1 |  81 || : 5
+  4  1 -8  5  6 |  53 
+ -2 -9 -6  3  2 | -71
+  3  0  4 -2 -4 |  21
+  7 -5  7 -4 -2 |  31
+
+  1  a  a  a  a |  a
+  4  1 -8  5  6 |  53 || -4·(1. Z.) 
+ -2 -9 -6  3  2 | -71 || +2·(1. Z.)
+  3  0  4 -2 -4 |  21 || -3·(1. Z.)
+  7 -5  7 -4 -2 |  31 || -7·(1. Z.)
+
+  1  a  a  a  a |   a
+  0  b  b  b  b |   b
+  0  c  c  c  c |   c
+  0  d  d  d  d |   d
+  0  e  e  e  e |   e
+
+2. Zeile:
+  1  a  a  a  a |   a
+  0  b  b  b  b |   b || :b
+  0  c  c  c  c |   c
+  0  d  d  d  d |   d
+  0  e  e  e  e |   e
+
+  1  a  a  a  a |   a
+  0  1  f  f  f |   f
+  0  c  c  c  c |   c || - c·(2. Z.)
+  0  d  d  d  d |   d || - d·(2. Z.)
+  0  e  e  e  e |   e || - e·(2. Z.)
+
+  1  a  a  a  a |   a
+  0  1  f  f  f |   f
+  0  0  g  g  g |   g
+  0  0  h  h  h |   h
+  0  0  i  i  i |
+
+...
+
+  1  a  a  a  a |  r_a
+  0  1  b  b  b |  r_b
+  0  0  1  c  c |  r_c
+  0  0  0  1  d |  r_d
+  0  0  0  0  1 |  r_e
+
+x₄ = r_e
+x₃ = r_d - d·x₄
+~~~
+
+Problem:
+- Auf der Diagonale entsteht eine Null (müssten dann durch 0 teilen!)
+
+## Gauß-Verfahren mit Pivotzeile
+
+Lösung:
+- Vertauschen diese Zeile mit einer anderen Folgezeile, die hier keine Null
+enthält.
+- Wählen die Zeilen mit betragsmäßig größtem Element
+- dann normales Gauß-Verfahren
---
+++ rust/float/matrixop/gauss.rs
@@ -0,0 +1,124 @@
+//! Matrix-Vector multiplication
+
+mod simple_input;
+use simple_input::Input;
+
+fn main() {
+    let mut inp = Input::new();
+    let matrix = read_matrix(&mut inp);
+    let m_size = matrix.len();
+    let rhs = read_vector(&mut inp, m_size);
+    println!("Matrix:");
+    show_matrix(&matrix);
+    println!("Rhs:");
+    show_vector(&rhs);
+    let x = solve_sle(&matrix, &rhs);
+    println!("Result:");
+    show_vector(&x);
+    // eval result → need copy of orig. matrix and rhs
+    let prod = mat_vec_mul(&matrix, &x);
+    let err = vec_diff(&prod, &rhs);
+    println!("Error:");
+    show_vector(&err);
+}
+
+fn read_matrix(inp: &mut Input) -> Vec<Vec<f64>> {
+    let size = inp.read_int();
+    let mut matrix: Vec<Vec<f64>> = Vec::new();
+    for _row in 0..size {
+        let mut mat_row: Vec<f64> = Vec::new();
+        for _col in 0..size {
+            let val = inp.read_float();
+            mat_row.push(val);
+        }
+        matrix.push(mat_row);
+    }
+    matrix
+}
+
+fn read_vector(inp: &mut Input, size: usize) -> Vec<f64> {
+    let mut vector: Vec<f64> = Vec::new();
+    for _ in 0..size {
+        let val = inp.read_float();
+        vector.push(val);
+    }
+    vector
+}
+
+fn show_matrix(matrix: &[Vec<f64>]) {
+    for row in matrix {
+        for value in row {
+            print!("{:8.2}", value);
+        }
+        println!();
+    }
+}
+
+fn show_vector(vector: &[f64]) {
+    for value in vector {
+        print!("{:8.2}", value);
+    }
+    println!();
+}
+
+fn mat_vec_mul(matrix: &[Vec<f64>], vector: &[f64]) -> Vec<f64> {
+    let mut res: Vec<f64> = Vec::new();
+    let size = matrix.len();
+    for row in 0..size {
+        // row = i
+        let mut sum = 0.0;
+        for col in 0..size {
+            // col = k
+            sum += matrix[row][col] * vector[col];
+        }
+        res.push(sum); // res[i]
+    }
+    res
+}
+
+fn vec_diff(v1: &[f64], v2: &[f64]) -> Vec<f64> {
+    // alternative: for (e1, e2) in v1.iter().zip(v2)
+    let size = v1.len();
+    let mut v_diff: Vec<f64> = Vec::new();
+    for idx in 0..size {
+        v_diff.push(v2[idx] - v1[idx]);
+    }
+    v_diff
+}
+
+fn solve_sle(matrix_ori: &Vec<Vec<f64>>, rhs_ori: &Vec<f64>) -> Vec<f64> {
+    // copy matrix_ori, rhs_ori
+    let mut m = matrix_ori.clone();
+    let mut rhs = rhs_ori.clone();
+    let size = m.len();
+    // forward pass
+    for row in 0..size {
+        let m_rr = m[row][row];
+        for col in row..size {
+            m[row][col] /= m_rr;
+        }
+        rhs[row] /= m_rr;
+        // following rows: subtract factor*row
+        for r_next in row + 1..size {
+            let q = m[r_next][row];
+            for col in row..size {
+                m[r_next][col] -= q * m[row][col];
+            }
+            rhs[r_next] -= q * rhs[row];
+        }
+        println!("row: {}", row);
+        show_matrix(&m);
+    }
+    // backward pass
+    let mut x = vec![0.0; size];
+    let mut row = size;
+    while row > 0 {
+        row -= 1;
+        let mut sum = 0.0;
+        for col in row + 1..size {
+            sum += m[row][col] * x[col];
+        }
+        x[row] = rhs[row] - sum;
+    }
+    x
+}
